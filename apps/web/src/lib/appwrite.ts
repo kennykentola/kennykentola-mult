f0ed04dbf1e4f9b28ef7cbef80413636e896cf65
User <user@example.com>
2026-07-04 17:23:49 +0100

    fix: export appwriteConfig and databases

--- apps/web/src/lib/appwrite.ts
+++ apps/web/src/lib/appwrite.ts
@@ -1,8 +1,14 @@
-import { Client, Account } from 'appwrite';
+import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client()
   .setEndpoint('https://fra.cloud.appwrite.io/v1')
   .setProject('kennykentolamult');
 
 export const account = new Account(client);
+export const databases = new Databases(client);
+
+export const appwriteConfig = {
+  databaseId: 'multicompany' // Using the same database ID configured elsewhere
+};
+
 export { client };
